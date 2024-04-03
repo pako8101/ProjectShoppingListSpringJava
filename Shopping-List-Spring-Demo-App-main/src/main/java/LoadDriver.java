a1094f592b533e32ddceb1a31bf5cf67e9b0caa1
pako8101 <pako810129@gmail.com>
2024-04-03 19:38:50 +0300

    Spring thymeleaf project

---
+++ Shopping-List-Spring-Demo-App-main/src/main/java/LoadDriver.java
@@ -0,0 +1,15 @@
+// Notice, do not import com.mysql.cj.jdbc.*
+// or you will have problems!
+
+class LoadDriver {
+    public static void main(String[] args) {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        }
+    }
+}
